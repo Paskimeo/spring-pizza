5ac2e0f41982bca79b7219cb54979f51959c5ec2
paskimeo <paskimeo@gmail.com>
2022-07-22 16:22:24 +0200

    aggiunto controller per add e delete

--- src/main/java/jana60/controller/pizzaController.java
+++ src/main/java/jana60/controller/pizzaController.java
@@ -3,16 +3,21 @@ package jana60.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.model.pizza;
 import jana60.repository.pizzaRepository;
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -41,6 +46,39 @@ public class pizzaController {
 	      repo.save(formPizza);
 	      return "redirect:/";
 	    }
+	    
+	 }  
+	 // request a http://localhost:8080/delete/2
+	    @GetMapping("/delete/{id}")
+	    public String delete(@PathVariable("id") Integer pizzaId, RedirectAttributes ra) {
+	    	java.util.Optional<pizza> result = repo.findById(pizzaId);
+	    	if (result.isPresent()) {
+	    	      // repo.deleteById(bookId);
+	    	      repo.delete(result.get());
+	    	      ra.addFlashAttribute("successMessage", "pizza " + result.get().getNome() + " deleted!");
+	    	      return "redirect:/";
+	    	    } else 
+	    	    {
+	    	      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+	    	          "Book con id " + pizzaId + " not present");
+	    }
+	    }
+	    	
+	    	 @GetMapping("/edit/{id}")
+	    	  public String edit(@PathVariable("id") Integer bookId, Model model) {
+	    	    java.util.Optional<pizza> result = repo.findById(bookId);
+	    	    // controllo se il Book con quell'id è presente
+	    	    if (result.isPresent()) {
+	    	      // preparo il template con al form passandogli il book trovato su db
+
+	    	      model.addAttribute("book", result.get());
+	    	      return "/book/edit";
+	    	    } else {
+	    	      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+	    	          "Book con id " + bookId + " not present");
+	    	    }
+	    	
+	    	
 	  }
 	
 }
--- src/main/java/jana60/repository/pizzaRepository.java
+++ src/main/java/jana60/repository/pizzaRepository.java
@@ -5,5 +5,8 @@ import jana60.model.pizza;
 
 
 public interface pizzaRepository extends CrudRepository<pizza, Integer>{
+	
+	//ti dice il quantitativo di copie gia presenti con quel nome, l'integer iniziale è il dato che torna
+	public Integer countByNome(String nome);
 
 }
